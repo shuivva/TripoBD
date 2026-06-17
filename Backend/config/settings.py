293e0c859fe7c54c3f11dcf168242e66a8fcf2e1
Sourav Biswas <sbiswas223635@bscse.uiu.ac.bd>
2026-06-17 14:09:42 +0600

    Add local development CORS ports

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -149,6 +149,10 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
+    'http://localhost:5175',
+    'http://127.0.0.1:5175',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
